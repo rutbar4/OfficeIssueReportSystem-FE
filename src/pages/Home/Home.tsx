45eb126f2119a6981e85f59759e3a86b8569758d
Lukrow <lukroza1@gmail.com>
2023-10-07 12:19:56 +0300

    Added Issue component, Added Icons folder

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
-
+import IssueCard from 'src/components/Issue'
 import WelcomeMessage from '../WelcomeMessage/WelcomeMessage';
 
 const Home = () => {
@@ -21,6 +21,14 @@ const Home = () => {
         Hello World!
       </Typography>
       <WelcomeMessage name={name}/>
+      <IssueCard
+        issueName="Example Issue"
+        issueDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        issueStatus="Open"
+        upvoteCount={10}
+        commentCount={5}
+        date={new Date().toLocaleDateString()}
+      />
     </Box>
   );
 };
