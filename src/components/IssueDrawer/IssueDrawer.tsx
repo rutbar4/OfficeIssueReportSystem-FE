48d1ca1a00a034b14651e04ac85e6f2d7f4bb64d
Rūta <ruta.b.bartkute@gmail.com>
2023-10-16 17:52:11 +0300

    small fix

--- src/components/IssueDrawer/IssueDrawer.tsx
+++ src/components/IssueDrawer/IssueDrawer.tsx
@@ -3,13 +3,11 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 
 export default function IssueDrawer({ wrapperSetState, state }) {
-  const list = () => <Box sx={{ width: 250 }}></Box>;
-
   return (
     <div>
       <React.Fragment key={'right'}>
         <Drawer anchor={'right'} open={state} onClose={() => wrapperSetState(false)}>
-          {list()}
+          <Box sx={{ width: 250 }}></Box>;
         </Drawer>
       </React.Fragment>
     </div>
