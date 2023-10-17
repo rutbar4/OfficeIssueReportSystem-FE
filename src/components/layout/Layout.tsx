774f29a20172cec0c8ea563ae6cf08e6aaaf77e8
Justinas Teselis <teselisjustinas@gmail.com>
2023-10-17 21:11:04 +0300

    SA2FTA-42 Added a user menu in TopHeader with Logout and My Profile options. Added an empty div above the content of the page, so the content doesn't go under the top header instantly.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,6 +10,8 @@ const Layout = (): JSX.Element => {
     <>
       <TopHeader />
       <Container maxWidth="xl">
+        {/* Used to make space under the header so the content doesn't go under it */}
+        <div style={{ paddingTop: '120px' }} />
         <Outlet />
       </Container>
     </>
