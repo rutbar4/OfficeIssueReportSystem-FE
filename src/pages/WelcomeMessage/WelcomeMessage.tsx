e840ac9bae9e0a7067e961fd0d02257582772f5f
TomasBaniulis <baniulis.tomas@gmail.com>
2023-10-17 13:17:07 +0300

    Report issue modal.  "Cancel" button - outlined not working

--- src/pages/WelcomeMessage/WelcomeMessage.tsx
+++ src/pages/WelcomeMessage/WelcomeMessage.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { Add } from '@mui/icons-material';
-
+import Button from '@mui/material/Button';
 import StyledButton from 'src/components/StyledButton/StyledButton';
+import {styled} from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import DialogContent from '@mui/material/DialogContent';
+import Divider from '@mui/material/Divider';
+import DialogActions from '@mui/material/DialogActions';
 
 type Props =  {
   name: string;
 }
 
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
 const WelcomeMessage: React.FC<Props> = ({ name }) => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // @ts-ignore
   return (
     <Box display="flex" alignItems="center" p={2}>
       <Box>
@@ -16,16 +44,51 @@ const WelcomeMessage: React.FC<Props> = ({ name }) => {
           Welcome, {name}
         </Typography>
         <Typography variant="h5" sx={{ color: '#6B706D', marginBottom: '10px' }}>
-          Discover, report and vote for office issuethat requires our attention and fix
+          Discover, report and vote for office issue that requires our attention and fix
           </Typography>
       </Box>
       <Box ml="auto">
-        <StyledButton buttonType='primary' buttonSize='medium' type='button'
+        <StyledButton onClick={handleClickOpen} buttonType='primary' buttonSize='medium' type='button'
           startIcon={<Add sx={{ fontSize: '24px' }} />}
         >
           Report Issue
         </StyledButton>
       </Box>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 3, p: 2 }} id="customized-dialog-title">
+          <Typography variant="h4" gutterBottom sx={{ color: '#0E166E' }}>
+            Report issue
+          </Typography>
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: 'black',
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent>
+          <Box sx = {{width: '500px'}}></Box>
+        </DialogContent>
+        <Divider/>
+        <DialogActions>
+          <StyledButton  buttonSize={"medium"} buttonType={"secondary"} type={"button"} onClick={handleClose}>
+            Cancel
+          </StyledButton>
+          <StyledButton   buttonSize={"medium"} buttonType={"primary"} type={"submit"}>
+            Report issue
+          </StyledButton>
+        </DialogActions>
+      </BootstrapDialog>
     </Box>
   );
 };
