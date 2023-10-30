206d4d0f30e96962c769d5217745ff1e671148b0
Lukrow <lukroza1@gmail.com>
2023-10-30 22:20:25 +0200

    Refactored some variable names

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/login";
+const login_URL = "http://localhost:8080/api/auth/login";
 
-const signIn = (username: string, password: string) => {
-    return axios.post(API_URL, { username, password },
+const signIn = (email: string, password: string) => {
+    return axios.post(login_URL, { email, password },
         )
         .then((response) => {
                 localStorage.setItem("user", JSON.stringify(response.data));
