941482fe5a5602488f02779aa40b77b7d79e6457
Lukrow <lukroza1@gmail.com>
2023-11-10 16:51:28 +0200

    Made DRY pattern based issue Reducer creator same as with Actions

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,20 +1,15 @@
 import { combineReducers } from 'redux';
-
-import issuesReducer from './issues/IssuesReducer';
 import authReducer from './signIn/authReducer';
-import openIssues from './issues/OpenIssuesReducer';
-import plannedIssues from './issues/PlannedIssuesReducer';
-import resolvedIssues from './issues/ResolvedIssuesReducer';
-import closedIssues from './issues/ClosedIssuesReducer';
-import userIssues from './issues/UserIssuesReducer';
+import { issuesReducer, openIssuesReducer, plannedIssuesReducer,
+   resolvedIssuesReducer, closedIssuesReducer, userIssuesReducer } from './issues/IssuesReducer';
 
 
 export default combineReducers({
   issues: issuesReducer,
   authentication: authReducer,
-  openIssues: openIssues,
-  plannedIssues: plannedIssues,
-  resolvedIssues: resolvedIssues,
-  closedIssues: closedIssues,
-  userIssues: userIssues,
+  openIssues: openIssuesReducer,
+  plannedIssues: plannedIssuesReducer,
+  resolvedIssues: resolvedIssuesReducer,
+  closedIssues: closedIssuesReducer,
+  userIssues: userIssuesReducer,
 });
