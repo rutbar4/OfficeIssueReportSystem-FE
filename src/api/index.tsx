e2695e215e14152fd3b6b34858850903174e00d1
SarunasJ-gif <sarunasj82@gmail.com>
2023-11-13 08:11:04 +0200

    SA2FTA-14: changes

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -11,7 +11,7 @@ const HTTP = axios.create(
 HTTP.interceptors.request.use(config =>{
   const jwtToken = store.getState().user.jwtToken;
   if(jwtToken){
-    config.headers.Authorization = `Barer ${jwtToken}`;
+    config.headers.Authorization = `Bearer ${jwtToken}`;
   }
   return config;
 });
