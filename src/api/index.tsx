2fbc897c8f1f49b3abc0e8d9b86e888098c5cf67
SarunasJ-gif <sarunasj82@gmail.com>
2023-11-18 11:58:58 +0200

    SA2FTA-14: deleted port

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,16 +1,15 @@
 // eslint-disable-next-line react/jsx-filename-extension
 import axios from 'axios';
-import { useSelector } from 'react-redux';
-import  { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+
 
 import backendConfig from './BackendConfig/BackendConfig';
-import store, { RootState } from '../store/store';
+import store from '../store/store';
 
 const HTTP = axios.create({
   baseURL: backendConfig.backendURL,
 });
 
-HTTP.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+HTTP.interceptors.request.use((config) => {
   const jwtToken = store.getState().user.jwt;
   if (jwtToken) {
     config.headers.Authorization = `Bearer ${jwtToken}`;
