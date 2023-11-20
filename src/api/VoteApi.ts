9982796d4afcb071ea8eae7b9358cf144dfadb2d
Rūta Bartkutė <ruta.b.bartkute@gmail.com>
2023-11-20 18:49:56 +0200

    Revert "Api clean up jwt"
    This reverts commit 6d554ecfccecee67d00ff15de6646db5539b7374.

--- src/api/VoteApi.ts
+++ src/api/VoteApi.ts
@@ -8,7 +8,12 @@ export const emptyVote: VoteModel = {
 
 export async function IsVoted(issueId: string) {
   try {
-    const response = await HTTP.get(`/vote/${issueId}`);
+    let jwt = store.getState().user.jwt;
+    const response = await HTTP.get(`/vote/${issueId}`, {
+      headers: {
+        Authorization: 'Bearer ' + jwt,
+      },
+    });
     return response.data.isVoted;
   } catch (error) {
     // reikia kad mygtukas irgi sureaguotu kai nepavyko -nepasispaudti ir išmesti alertą
@@ -17,7 +22,12 @@ export async function IsVoted(issueId: string) {
 }
 export async function GetVoteCount(issueId) {
   try {
-    const response = await HTTP.get(`/vote/count/${issueId}`);
+    let jwt = store.getState().user.jwt;
+    const response = await HTTP.get(`/vote/count/${issueId}`, {
+      headers: {
+        Authorization: 'Bearer ' + jwt,
+      },
+    });
     return response.data.count;
   } catch (error) {
     throw new Error('Failed to fetch  issue count');
@@ -26,7 +36,12 @@ export async function GetVoteCount(issueId) {
 
 export async function PostVote(issueId) {
   try {
-    const response = await HTTP.post(`/vote/${issueId}`, null);
+    let jwt = store.getState().user.jwt;
+    const response = await HTTP.post(`/vote/${issueId}`, null, {
+      headers: {
+        Authorization: 'Bearer ' + jwt,
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error(`Failed to create a vote for the issue: ${issueId}`);
@@ -35,7 +50,12 @@ export async function PostVote(issueId) {
 
 export async function DeleteVote(issueId) {
   try {
-    const response = await HTTP.delete(`/vote/${issueId}`);
+    let jwt = store.getState().user.jwt;
+    const response = await HTTP.delete(`/vote/${issueId}`, {
+      headers: {
+        Authorization: 'Bearer ' + jwt,
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error('Failed to delete issue vote');
