8585451789485d6a2ec1b07ed60629f33c74333b
Rūta Bartkutė <ruta.b.bartkute@gmail.com>
2023-12-03 01:11:55 +0200

    default: All employees

--- src/components/filters/EmployeeSelectMenu.tsx
+++ src/components/filters/EmployeeSelectMenu.tsx
@@ -4,7 +4,6 @@ import Autocomplete, { autocompleteClasses } from '@mui/material/Autocomplete';
 
 import { fetchAllUsers } from 'src/api/UserListApi';
 import { User } from 'src/models/BasicUserModel';
-import { COLORS } from 'src/values/colors.js';
 
 export default function EmployeeSelectMenu({ setSelectedUser }) {
   const [users, setUsers] = React.useState<User[]>([]);
@@ -28,15 +27,15 @@ export default function EmployeeSelectMenu({ setSelectedUser }) {
         disablePortal
         id="employee-selection"
         options={users}
+        defaultValue={{ fullName: 'All Employees', id: '' }}
         getOptionLabel={(user: User) => user.fullName}
-        sx={{ width: 200,[`&& .${autocompleteClasses.inputRoot}`]: {
-          paddingRight: 2
-        } }}
+        sx={{ width: 200 }}
         size="small"
         renderInput={(params) => (
           <TextField
             {...params}
-            label="All Employees"
+            label="Employee:"
+            placeholder="Employee"
             sx={{
               fieldset: {
                 borderRadius: '20px',
